569520ebfc3386c40f7dce4659a684654de49906
Marshal38 <vladislav.zhukov52@gmail.com>
2026-02-22 23:10:53 +0500

    delete class css

--- src/ThailandFlag.js
+++ src/ThailandFlag.js
@@ -3,7 +3,7 @@ import "./ThailandFlag.css";
 function ThailandFlag() {
   return (
     <div className="flag-scene">
-      <div className="flag thailand">
+      <div className="flag">
         <span className="stripe red"></span>
         <span className="stripe white"></span>
         <span className="stripe blue"></span>
