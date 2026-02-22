14657dc6b010c6750aaf7553f51dc249d138de04
Marshal38 <vladislav.zhukov52@gmail.com>
2026-02-22 23:39:19 +0500

    central place

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import Timer from "./Timer";
 import ThailandFlag from "./ThailandFlag";
+import "./index.css";
 
 function App() {
   return (
-    <div>
+    <main>
       <h1>ОСТАЛОСЬ ДО ВСТРЕЧИ:</h1>
       <Timer targetDate={"2026-03-10T14:00"} />
       <ThailandFlag />
-    </div>
+    </main>
   );
 }
 
